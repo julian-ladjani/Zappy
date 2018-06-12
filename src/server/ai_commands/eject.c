45fa1081e23516b70d2b0b7f3c9133dee0b619a6
gregory.eple <gregory.eple@localhost.localdomain>
2018-06-12 17:03:49 +0200

    [server] added protorype for commands

---
+++ src/server/ai_commands/eject.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2018
+** PSU_zappy_2017
+** File description:
+** eject.c
+*/
+
+#include "server_struct.h"
+
+uint8_t srv_cmd_eject(server_config_s *server,
+			server_user_t *user, char *args)
+{
+	return (1);
+}
---
+++ src/server/graphic_commands/msz.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2018
+** PSU_zappy_2017
+** File description:
+** msz.c
+*/
+
+#include "server_struct.h"
+
+uint8_t srv_cmd_msz(server_config_s *server,
+			server_user_t *user, char *args)
+{
+	return (1);
+}
