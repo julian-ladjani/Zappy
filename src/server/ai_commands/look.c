760accfea23af7afbdd5228a2bc8286390e73e6d
gregory.eple <gregory.eple@localhost.localdomain>
2018-06-12 17:03:49 +0200

    [server] added protorype for commands

---
+++ src/server/ai_commands/look.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2018
+** PSU_zappy_2017
+** File description:
+** look.c
+*/
+
+#include "server_struct.h"
+
+uint8_t srv_cmd_look(server_config_s *server,
+			server_user_t *user, char *args)
+{
+	return (1);
+}
