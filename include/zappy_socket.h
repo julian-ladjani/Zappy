3bad14d5b6798e63b07bab083a97f3a588cc7ffb
julian ladjani <julian.ladjani@epitech.eu>
2018-06-12 14:37:07 +0200

    Add sockets

--- include/zappy_socket.h
+++ include/zappy_socket.h
@@ -16,5 +16,7 @@ typedef struct zappy_socket_s {
 	char *ip;
 } zappy_socket_t;
 
+void cleanup_socket(zappy_socket_t *master);
+zappy_socket_t *initialise_socket(int port);
 
 #endif //PSU_ZAPPY_2017_SOCKET_H
