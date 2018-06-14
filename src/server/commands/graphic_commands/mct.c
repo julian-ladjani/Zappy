3a59f785ba6923c9e219294e93004efd134f56f5
gregory.eple <gregory.eple@localhost.localdomain>
2018-06-14 16:09:26 +0200

    [server] implemented bct, mct and msz commands

--- src/server/commands/graphic_commands/mct.c
+++ src/server/commands/graphic_commands/mct.c
@@ -7,8 +7,19 @@
 
 #include "server_struct.h"
 
-uint8_t srv_cmd_mct(server_config_t *server,
-			server_user_t *user, cmdparams_t *cmd)
+uint8_t srv_cmd_mct(server_config_t *server, server_user_t *user,
+			__attribute__((unused))cmdparams_t *cmd)
 {
-	return (1);
+	char *ressources;
+
+	for (unsigned int y = 0; y < server->map->height; ++y) {
+		for (unsigned int x = 0; x < server->map->width; ++x) {
+			ressources = tile_to_str_quantity(
+				server->map->tiles[y][x]);
+			dprintf(user->fd, "bct %u %u %s\n", x, y, ressources);
+			if (ressources)
+				free(ressources);
+		}
+	}
+	return (0);
 }
