bcea950b0183ce74acd02c7b10f394ca98b537df
julian ladjani <julian.ladjani@epitech.eu>
2018-06-15 15:52:16 +0200

    [Arg Parser] Fix stop issue

---
+++ include/utils.h
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2018
+** PSU_zappy_2017
+** File description:
+** 	utils header file
+*/
+
+#ifndef PSU_ZAPPY_2017_UTILS_H
+#define PSU_ZAPPY_2017_UTILS_H
+
+#endif //PSU_ZAPPY_2017_UTILS_H
---
+++ src/common/utils/tab_cpy.c
@@ -0,0 +1,7 @@
+/*
+** EPITECH PROJECT, 2018
+** PSU_zappy_2017
+** File description:
+** 	tab_cpy source file
+*/
+
