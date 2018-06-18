db1c23c75de8caaf317a2ba4ed36b7c50b5c238d
gregory.eple <gregory.eple@localhost.localdomain>
2018-06-18 18:23:50 +0200

    [Merge]

--- src/server/commands/ai_commands/fork.c
+++ src/server/commands/ai_commands/fork.c
@@ -5,15 +5,15 @@
 ** fork.c
 */
 
-#include "server_struct.h"
+#include "server_function.h"
 
 uint8_t srv_cmd_fork(server_config_t *server, server_user_t *user,
 	__attribute__((unused))cmdparams_t *cmd)
 {
 	char *msg;
 
-	asprintf(&msg, "pfk %d\nenw %d %d %u %u\n", user->id, -1,
-		user->id, user->x, user->y);
+	asprintf(&msg, "pfk %d\nenw %d %d %lu %lu\n", user->id, -1,
+		user->id, user->pos.x, user->pos.y);
 	send_msg_to_all_graphic(server, msg);
 	dprintf(user->fd, "ok\n");
 	user->wait += 42;
