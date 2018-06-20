3abbde5fe9dc886402817579948ef1e98ea40efe
yannick <yannick.suc@epitech.eu>
2018-06-20 15:08:11 +0200

    [AiClient] Add macro for debugging

--- src/ai_client/ai/searcher.c
+++ src/ai_client/ai/searcher.c
@@ -18,11 +18,16 @@ static void prepare_incantation(clt_config_t *clt)
 			tile = map_get_tile(clt->map,
 					    clt->specs->x, clt->specs->y);
 			send_request(TAKE, clt, i);
-			print_map(clt->map);
+			if (ZAPPY_DEBUG)
+				print_map(clt->map);
 		}
 	}
+	printf("Incantation ?\n");
+	send_request(INVENTORY, clt);
 	if (condition_pre_incantation(clt)) {
+		printf("Incantation ? 2\n");
 		if ((*tile)[PLAYER] == (*ref)[PLAYER]) {
+			printf("Incantation ! 3\n");
 			send_request(BROADCAST, clt, "incantation:start:%d",
 					clt->specs->level);
 			send_request(INCANTATION, clt);
@@ -36,15 +41,19 @@ int ai_searcher(clt_config_t *clt)
 	tile_t *tile;
 
 	send_request(LOOK, clt);
-	print_map(clt->map);
+	if (ZAPPY_DEBUG)
+		print_map(clt->map);
 	tile = map_get_tile(clt->map, clt->specs->x, clt->specs->y);
 	while ((*tile)[FOOD] != 0)
 		send_request(TAKE, clt, FOOD);
 	if (tilecmp(tile, (tile_t *)
 		(INCANTATION_OBJ + clt->specs->level)) >= 0) {
+		printf("IF\n");
 		prepare_incantation(clt);
+		return (ZAPPY_EXIT_SUCCESS);
 	}
-	print_map(clt->map);
+	if (ZAPPY_DEBUG)
+		print_map(clt->map);
 	if (clt->specs->forwarding >= clt->map->width) {
 		send_request(RIGHT, clt);
 		send_request(FORWARD, clt);
