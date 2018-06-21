ff9bf09a10249c15cb094dd9d1bfb574603c998c
yannick <yannick.suc@epitech.eu>
2018-06-21 17:09:34 +0200

    [AiClient] set tileactions to another file

--- src/ai_client/ai/searcher.c
+++ src/ai_client/ai/searcher.c
@@ -7,47 +7,6 @@
 
 #include "client.h"
 
-static void clear_tile_from_ref(clt_config_t *clt, tile_t *tile, tile_t *ref)
-{
-	for (int i = 1; i < 7; ++i) {
-		send_request(LOOK, clt);
-		while ((*tile)[i] > (*ref)[i]) {
-			send_request(TAKE, clt, i);
-			if (ZAPPY_DEBUG)
-				print_map(clt->map);
-		}
-	}
-}
-
-static void fill_tile_from_inv(clt_config_t *clt, tile_t *tile, tile_t *ref)
-{
-	send_request(INVENTORY, clt);
-	for (int i = 1; i < 7; ++i) {
-		send_request(LOOK, clt);
-		while ((*tile)[i] < (*ref)[i] && clt->specs->inventory[i] > 0) {
-			send_request(SET, clt, i);
-			if (ZAPPY_DEBUG)
-				print_map(clt->map);
-		}
-	}
-}
-
-static void take_obj_from_ref(clt_config_t *clt, tile_t *ref)
-{
-	tile_t *tile = map_get_tile(clt->map, clt->specs->x, clt->specs->y);
-
-	send_request(INVENTORY, clt);
-	for (int i = 1; i < 7; ++i) {
-		send_request(LOOK, clt);
-		while ((*tile)[i] != 0 &&
-			clt->specs->inventory[i] < (*ref)[i]) {
-			send_request(TAKE, clt, i);
-			if (ZAPPY_DEBUG)
-				print_map(clt->map);
-		}
-	}
-}
-
 static int check_obj_for_incantation(clt_config_t *clt)
 {
 	tile_t *tile = map_get_tile(clt->map, clt->specs->x, clt->specs->y);
---
+++ src/ai_client/tile_actions.c
@@ -0,0 +1,49 @@
+/*
+** EPITECH PROJECT, 2018
+** PSU_zappy_2017
+** File description:
+** tile_actions.c
+*/
+
+#include "client.h"
+
+void clear_tile_from_ref(clt_config_t *clt, tile_t *tile, tile_t *ref)
+{
+	for (int i = 1; i < 7; ++i) {
+		send_request(LOOK, clt);
+		while ((*tile)[i] > (*ref)[i]) {
+			send_request(TAKE, clt, i);
+			if (ZAPPY_DEBUG)
+				print_map(clt->map);
+		}
+	}
+}
+
+void fill_tile_from_inv(clt_config_t *clt, tile_t *tile, tile_t *ref)
+{
+	send_request(INVENTORY, clt);
+	for (int i = 1; i < 7; ++i) {
+		send_request(LOOK, clt);
+		while ((*tile)[i] < (*ref)[i] && clt->specs->inventory[i] > 0) {
+			send_request(SET, clt, i);
+			if (ZAPPY_DEBUG)
+				print_map(clt->map);
+		}
+	}
+}
+
+void take_obj_from_ref(clt_config_t *clt, tile_t *ref)
+{
+	tile_t *tile = map_get_tile(clt->map, clt->specs->x, clt->specs->y);
+
+	send_request(INVENTORY, clt);
+	for (int i = 1; i < 7; ++i) {
+		send_request(LOOK, clt);
+		while ((*tile)[i] != 0 &&
+		       clt->specs->inventory[i] < (*ref)[i]) {
+			send_request(TAKE, clt, i);
+			if (ZAPPY_DEBUG)
+				print_map(clt->map);
+		}
+	}
+}
