74c7bddd1f6859f25ed05cf7b28a6931e5206c1d
lucas.depret <lucas.depret@epitech.eu>
2018-06-21 18:00:33 +0200

    [Unity client] Parcing command + Ressource, Player display + Player Move and Dead

--- src/unity_client/Assets/MainMenu.cs
+++ src/unity_client/Assets/MainMenu.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
 using UnityEngine;
 
 public class MainMenu : MonoBehaviour {
@@ -7,23 +9,33 @@ public class MainMenu : MonoBehaviour {
 		private AudioSource musicMain;
 		private AudioSource musicConnect;
 		private Canvas Connect;
+		private Canvas Fail;
+		private Canvas Main;
+		private string host;
+		private int port;
 	// Use this for initialization
 	private void Start() {
 		GameObject obj;
 		GameObject obj2;
+		GameObject obj3;
 		obj = GameObject.Find("Connection");
 		obj2 = GameObject.Find("Menu");
-		musicMain = obj.GetComponent<AudioSource>();
-		musicConnect = obj2.GetComponent<AudioSource>();
+		obj3 = GameObject.Find("Fail");
+		musicConnect = obj.GetComponent<AudioSource>();
+		musicMain = obj2.GetComponent<AudioSource>();
 		Connect = obj.GetComponent<Canvas>();
+		Fail = obj3.GetComponent<Canvas>();
+		Main = obj2.GetComponent<Canvas>();
 		Connect.enabled = false;
-		musicMain.Stop();
+		Fail.enabled = false;
+		Main.enabled = true;
+		musicConnect.Stop();
 	}
 
 	private void Connection() {
 		Connect.enabled = true;
-		musicMain.Play();
-		musicConnect.Stop();
+		musicConnect.Play();
+		musicMain.Pause();
 	}
 	// Update is called once per frame
 	private void Quit () {
