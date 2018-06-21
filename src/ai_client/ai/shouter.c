82ae4e9cd6a21e5deede80fb768befe56a2df7b0
yannick <yannick.suc@epitech.eu>
2018-06-21 17:01:38 +0200

    [AiClient] Add shouter Ai

--- src/ai_client/ai/shouter.c
+++ src/ai_client/ai/shouter.c
@@ -11,7 +11,7 @@ int ai_shouter(clt_config_t *clt)
 {
 	tile_t *tile = map_get_tile(clt->map, clt->specs->x, clt->specs->y);
 
-	while ((*tile)[PLAYER] != (size_t) INCANTATION_OBJ[PLAYER]) {
+	if ((*tile)[PLAYER] != (size_t) INCANTATION_OBJ[PLAYER]) {
 		send_request(BROADCAST, clt, "incantation;need_players;%d",
 				clt->specs->level);
 		send_request(LOOK, clt);
@@ -19,7 +19,11 @@ int ai_shouter(clt_config_t *clt)
 		if (tilecmp(tile, (tile_t *)
 			(INCANTATION_OBJ + clt->specs->level)) != 0)
 			clt->specs->ai_mode = SEARCHER;
+		return (ZAPPY_EXIT_SUCCESS);
 	}
-
+	send_request(BROADCAST, clt, "incantation:start:%d",
+			clt->specs->level);
+	send_request(INCANTATION, clt);
+	clt->specs->ai_mode = SEARCHER;
 	return (ZAPPY_EXIT_SUCCESS);
 }
