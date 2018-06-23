1903631b930683e0e2e54dbc0f535c74f98d1595
gregory.eple <gregory.eple@localhost.localdomain>
2018-06-24 01:28:18 +0200

    [server] vector orientation calculate with angles

--- src/common/map/map_orientation.c
+++ src/common/map/map_orientation.c
@@ -11,20 +11,25 @@
 uint8_t map_get_orientation(vec_t dir)
 {
 	double angle;
-	uint8_t piece;
 
 	if (dir.x == 0 && dir.y == 0)
 		return (0);
 	angle = atan2(dir.y, dir.x);
-	if (angle < 0)
-		angle += 2 * M_PI;
-	piece = (uint8_t) (8 * angle / (2 * M_PI));
-	piece = (uint8_t) (piece >= 8 ? 8 : (8 - (piece + 1)));
-	if (piece <= 2 || piece == 4 || piece == 5)
-		return ((uint8_t) (6 - piece));
-	if (piece == 6 || piece == 8)
-		return (piece == 6 ? 8 : 6);
-	return (piece);
+	if (angle <= M_PI / 2 + M_PI / 8 && angle >= M_PI / 2 - M_PI / 8)
+		return (1);
+	if (angle <= (3 * M_PI) / 4 + M_PI / 8 && angle >= (3 * M_PI) / 4 - M_PI / 8)
+		return (2);
+	if (angle >= M_PI - M_PI / 8 || angle <= -M_PI + M_PI / 8)
+		return (3);
+	if (angle <= (-3 * M_PI) / 4 + M_PI / 8 && angle >= (-3 * M_PI) / 4 - M_PI / 8)
+		return (4);
+	if (angle <= -M_PI / 2 + M_PI / 8 && angle >= -M_PI / 2 - M_PI / 8)
+		return (5);
+	if (angle <= -M_PI / 4 + M_PI / 8 && angle >= -M_PI / 4 - M_PI / 8)
+		return (6);
+	if (angle <= M_PI / 8 && angle >= -M_PI / 8)
+		return (7);
+	return (8);
 }
 
 uint8_t map_rotate_orientation(cardinal_dir_t dir, uint8_t orientation)
