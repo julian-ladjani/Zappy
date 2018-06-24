00447a072b899e2f1a283e3888588c427e279223
lucas.depret <lucas.depret@epitech.eu>
2018-06-24 15:33:52 +0200

    [GUI client] remake quantity chunks

--- src/unity_client/Assets/Map.cs
+++ src/unity_client/Assets/Map.cs
@@ -9,21 +9,29 @@ public class Map {
 	public List<List<Chunk>> chunks{get; set;}
 	public Canvas RessourceUI{get; set;}
 	public Text[] TextUI{get;set;}
+	public MeshRenderer[] MeshUI{get;set;}
 	public Map() {
 		chunks = new List<List<Chunk>>();
 		RessourceUI = GameObject.Find("RessourceUI").GetComponent<Canvas>();
 		TextUI = GameObject.Find("Ressource").GetComponentsInChildren<Text>();
+		MeshUI = GameObject.Find("Ressource").GetComponentsInChildren<MeshRenderer>();
 	}
 
 	public void DisplayRessource(float X, float Y) {
 		if(X != -1) {
 			RessourceUI.enabled = true;
-			for(int i = 0; i < chunks[(int)X][(int)Y].Quantity.Count ; i++){
-				TextUI[i].text = chunks[(int)X][(int)Y].Quantity[i].ToString();
+			for(int i = 0; i < chunks[(int)X][(int)Y].Quantity.Count ; i++) {
+				TextUI[i].text = ":" + chunks[(int)X][(int)Y].Quantity[i].ToString();
+			}
+			for(int i = 0; i < MeshUI.Length; i++) {
+				MeshUI[i].enabled = true;
 			}
 		}
 		else {
 			RessourceUI.enabled = false;
+			for(int i = 0; i < MeshUI.Length; i++) {
+				MeshUI[i].enabled = false;
+			}
 		}
 	}
 }
